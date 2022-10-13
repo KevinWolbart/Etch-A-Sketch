582168351e9d8c8d55ad13cdb89275198c31f30d
Kevin Wolbart <kevinwolbart@gmail.com>
2022-10-13 16:08:51 -0500

    Finished -  pre extra credit

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
 const container = document.querySelector(".container");
-function createGrid(num) {
+function createGrid(num = 16) {
     for (let i = 0; i < (num * num); i++) {
         let div = document.createElement("div");
         div.classList.add("child");
-        div.style.width = (960 / num) + "px";
-        div.style.height = (960 / num) + "px";
+        div.style.width = (500 / num) + "px";
+        div.style.height = (500 / num) + "px";
         div.addEventListener("mouseover", hover);
         container.appendChild(div);
     }
 }
 
-createGrid(40);
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+function newGrid() {
+    let num = Number(prompt("How large do you want your grid (max is 100)", 16));
+    if (num === 0 || num > 100) {
+        alert("Invalid number entered");
+        return;
+    }
+    clearGrid();
+    createGrid(num);
+}
+
 
 function hover() {
     this.classList.add("newColor");
-    setTimeout(() => { this.classList.remove("newColor"); }, 500);
+    //setTimeout(() => { this.classList.remove("newColor"); }, 500);
 }
+
+createGrid();
